4c10224994d635832d264131ed2104ce6c57b352
Marvyn <marvyn.orourke@thetribe.io>
2023-06-07 11:00:16 +0200

    feat: 🎉 Start front architecture

---
+++ app/src/adapter/DomainHttpAdapter.ts
@@ -0,0 +1,44 @@
+import BaseHttpAdapter, { Body } from './BaseHttpAdapter';
+
+export interface DomainHttpAdapterInterface {
+    get: (path: string, args?: RequestInit) => Promise<Response>;
+    post: (path: string, body: Body, args?: RequestInit, contentType?: string) => Promise<Response>;
+    put: (path: string, body: Body, args?: RequestInit) => Promise<Response>;
+    delete: (path: string, args?: RequestInit) => Promise<Response>;
+}
+
+class DomainHttpAdapter extends BaseHttpAdapter implements DomainHttpAdapterInterface {
+    accessToken: string = '';
+
+    apiUrl: string = '';
+
+    constructor(apiUrl: string, accessToken: string) {
+        super();
+        this.accessToken = accessToken;
+        this.apiUrl = apiUrl;
+    }
+
+    private getHeaders(): any {
+        return {
+            Authorization: `Bearer ${this.accessToken}`,
+        };
+    }
+
+    async get(path: string, args: RequestInit = {}): Promise<Response> {
+        return super.get(`${this.apiUrl}${path}`, { ...args, headers: this.getHeaders() });
+    }
+
+    async delete(path: string, args: RequestInit = {}): Promise<Response> {
+        return super.delete(`${this.apiUrl}${path}`, { ...args, headers: this.getHeaders() });
+    }
+
+    async post(path: string, body: Body, args: RequestInit = {}, contentType = 'application/json'): Promise<Response> {
+        return super.post(`${this.apiUrl}${path}`, body, { ...args, headers: this.getHeaders() }, contentType);
+    }
+
+    async put(path: string, body: Body, args: RequestInit = {}): Promise<Response> {
+        return super.put(`${this.apiUrl}${path}`, body, { ...args, headers: this.getHeaders() });
+    }
+}
+
+export default DomainHttpAdapter;
