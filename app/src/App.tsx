41d988fac80f120c6843505a9259c2118060e218
Marvyn <marvyn.orourke@thetribe.io>
2023-06-07 11:03:38 +0200

    feat: Add offline router on App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,5 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonApp, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -21,21 +19,14 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import { LoginPage } from './pages/Login';
+import OfflineRouter from './presentation/router/OfflineRouter';
 
 setupIonicReact();
 
 const App: React.FC = () => (
     <IonApp>
         <IonReactRouter>
-            <IonRouterOutlet>
-                <Route exact path="/home">
-                    <Home />
-                </Route>
-                <Route exact path="/">
-                    <LoginPage />
-                </Route>
-            </IonRouterOutlet>
+                <OfflineRouter />
         </IonReactRouter>
     </IonApp>
 );
