2aefa68cc48a93760918738292147e0dc46f2ee4
Marvyn <marvyn.orourke@thetribe.io>
2023-06-07 11:22:36 +0200

    feat: Get language tag

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,8 @@
 import { IonApp, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import OfflineRouter from './presentation/router/OfflineRouter';
+import { useEffect } from 'react';
+import { Device } from '@capacitor/device';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -19,16 +22,26 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import OfflineRouter from './presentation/router/OfflineRouter';
 
 setupIonicReact();
 
-const App: React.FC = () => (
-    <IonApp>
-        <IonReactRouter>
+const App: React.FC = () => {
+    useEffect(() => {
+        const getLanguage = async () => {
+            const language = await Device.getLanguageCode();
+            //TODO: In next PR, add langage to i18n adapter
+            console.log(language.value);
+        };
+        getLanguage();
+    }, []);
+
+    return (
+        <IonApp>
+            <IonReactRouter>
                 <OfflineRouter />
-        </IonReactRouter>
-    </IonApp>
-);
+            </IonReactRouter>
+        </IonApp>
+    );
+};
 
 export default App;
