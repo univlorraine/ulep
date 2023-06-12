dffc83d9bd6ed5db6ced6bba0c2a02199636b2c3
Marvyn <marvyn.orourke@thetribe.io>
2023-06-12 16:31:48 +0200

    feat: Add button to change language

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,16 +1,26 @@
 import simpleRestProvider from 'ra-data-simple-rest';
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, AppBar, Toolbar, TitlePortal, LocalesMenuButton } from 'react-admin';
 import LoginPage from './auth/LoginPage';
-import authProvider from './providers/authProvider';
+import authProvider, { httpClient } from './providers/authProvider';
 import i18nProvider from './providers/i18nProvider';
 import UserList from './user/UserList';
+
+export const ULAppBar = () => (
+    <AppBar>
+        <Toolbar>
+            <TitlePortal />
+            <LocalesMenuButton />
+        </Toolbar>
+    </AppBar>
+);
 
 const App = () => (
     <Admin
         authProvider={authProvider()}
-        dataProvider={simpleRestProvider('/')}
+        dataProvider={simpleRestProvider(`${process.env.REACT_APP_API_URL}`, httpClient)}
         i18nProvider={i18nProvider}
+        layout={ULAppBar}
         loginPage={LoginPage}
     >
         <Resource list={UserList} name="users" />
