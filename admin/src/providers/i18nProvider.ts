5f1ea3de7e1d69b9af895e3cb50dcf2b93c28304
Marvyn <marvyn.orourke@thetribe.io>
2023-06-12 15:06:00 +0200

    feat: add custom translation

--- admin/src/providers/i18nProvider.ts
+++ admin/src/providers/i18nProvider.ts
@@ -2,9 +2,11 @@ import polyglotI18nProvider from 'ra-i18n-polyglot';
 import en from 'ra-language-english';
 import fr from 'ra-language-french';
 import { resolveBrowserLocale } from 'react-admin';
+import customEN from '../locales/en.json';
+import customFR from '../locales/fr.json';
 
 const i18nProvider = polyglotI18nProvider(
-    (locale) => (locale === 'fr' ? fr : en),
+    (locale) => (locale === 'fr' ? { ...fr, ...customFR } : { ...en, ...customEN }),
     resolveBrowserLocale(),
     [{ locale: 'en', name: 'English' }, { locale: 'fr', name: 'Français' }],
 );
