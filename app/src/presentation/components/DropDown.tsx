728489d0edefc620ca30dc5f36ed88180a2d8c41
Marvyn <marvyn.orourke@thetribe.io>
2023-07-11 11:07:46 +0200

    feat: End of UI / UX signup page

--- app/src/presentation/components/DropDown.tsx
+++ app/src/presentation/components/DropDown.tsx
@@ -7,37 +7,46 @@ interface DropDownItem {
 }
 
 interface DropdownProps {
+    onChange: (value: any) => void;
     options: DropDownItem[];
+    placeholder?: string | null;
+    title?: string | null;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ options }) => {
+const Dropdown: React.FC<DropdownProps> = ({ onChange, options, placeholder, title }) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [selectedOption, setSelectedOption] = useState<DropDownItem>(options[0]);
+    const [selectedOption, setSelectedOption] = useState<DropDownItem | undefined>(
+        !placeholder ? options[0] : undefined
+    );
 
     const handleOptionClick = (item: DropDownItem) => {
         setSelectedOption(item);
+        onChange(item.value);
         setIsOpen(false);
     };
 
     return (
-        <div className={styles.container}>
-            <button className={styles.button} onClick={() => setIsOpen(!isOpen)}>
-                {selectedOption.title}
-                <img alt="arrow-down" src="/public/assets/arrow-down.svg" />
-            </button>
-            {isOpen && (
-                <div className={styles.menu}>
-                    {options.map((option) => {
-                        return (
-                            <div key={option.value} className={styles.submenu}>
-                                <button className={styles.item} onClick={() => handleOptionClick(option)}>
-                                    {option.title}
-                                </button>
-                            </div>
-                        );
-                    })}
-                </div>
-            )}
+        <div>
+            <p className={styles.title}>{title}</p>
+            <div className={styles.container}>
+                <button className={styles.button} onClick={() => setIsOpen(!isOpen)}>
+                    {selectedOption ? selectedOption.title : placeholder}
+                    <img alt="arrow-down" src="/public/assets/arrow-down.svg" />
+                </button>
+                {isOpen && (
+                    <div className={styles.menu}>
+                        {options.map((option) => {
+                            return (
+                                <div key={option.value} className={styles.submenu}>
+                                    <button className={styles.item} onClick={() => handleOptionClick(option)}>
+                                        {option.title}
+                                    </button>
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
--- app/src/presentation/pages/SignUpPage.tsx
+++ app/src/presentation/pages/SignUpPage.tsx
@@ -6,25 +6,27 @@ import roles from '../../domain/entities/roles';
 import Dropdown from '../components/DropDown';
 import Header from '../components/Header';
 import RadioButton from '../components/RadioButton';
+import TextInput from '../components/TextInput';
 import styles from './SignUpPage.module.css';
 
 const SignUpPage: React.FC = () => {
     const { t } = useTranslation();
     const history = useHistory();
+    const [country, setCountry] = useState<string>('');
+    const [department, setDepartment] = useState<string>('');
+    const [diplome, setDiplome] = useState<string>('');
     const [selectedRole, setSelectedRole] = useState<roles>();
-
-    const handleSelectChange = (role: roles) => {
-        setSelectedRole(role);
-    };
+    const [staffFunction, setStaffFunction] = useState<string>('');
+    const [university, setUniversity] = useState<string>('');
 
     return (
         <IonPage>
             <Header progressColor="#FDEE66" progressPercentage={12} title={t('global.create_account_title')} />
             <IonContent>
                 <div className={styles.container}>
-                    <h1 className={styles.title}>Mon profil</h1>
+                    <h1 className={styles.title}>{t('signup_page.title')}</h1>
 
-                    <h2 className={styles.subtitle}>Mon profil</h2>
+                    <h2 className={styles.subtitle}>{t('signup_page.profile_title')}</h2>
 
                     <RadioButton
                         isSelected={selectedRole === 'STUDENT'}
@@ -37,28 +39,50 @@ const SignUpPage: React.FC = () => {
                         name={t('signup_page.staff_role')}
                     />
 
-                    <p className={styles.subtitle}>Pays</p>
-                    <Dropdown options={[{ title: 'a', value: 'a' }]} />
+                    <Dropdown
+                        onChange={setCountry}
+                        options={[{ title: 'a', value: 'a' }]}
+                        placeholder={t('signup_page.country_placeholder')}
+                        title={t('global.country')}
+                    />
 
-                    <p className={styles.subtitle}>Etablissement</p>
-                    <Dropdown options={[{ title: 'a', value: 'a' }]} />
+                    <Dropdown
+                        onChange={setUniversity}
+                        options={[{ title: 'a', value: 'a' }]}
+                        title={t('signup_page.university_title')}
+                    />
 
-                    <button className="tertiary-button large-margin-top" onClick={() => null}>
-                        M’authentifier
-                    </button>
-
-                    {selectedRole && <div className={styles.separator} />}
-
-                    {selectedRole === 'STAFF' && <div></div>}
-
-                    {!selectedRole && (
-                        <p className={styles.information}>
-                            Votre université n'est pas encore partenaire de l'Université de Lorraine ? Contactez-nous à
-                            email@contact.fr
-                        </p>
+                    {selectedRole !== 'STAFF' && (
+                        <button className="tertiary-button large-margin-top" onClick={() => null}>
+                            {t('signup_page.sso_button')}
+                        </button>
                     )}
-                    <button className="primary-button" onClick={() => null}>
-                        Valider et continuer
+
+                    {selectedRole === 'STUDENT' && <div className={styles.separator} />}
+
+                    {selectedRole && (
+                        <TextInput
+                            onChange={setDepartment}
+                            title={t('signup_page.department_title')}
+                            value={department}
+                        />
+                    )}
+
+                    {selectedRole === 'STAFF' && (
+                        <TextInput
+                            onChange={setStaffFunction}
+                            title={t('signup_page.function_title')}
+                            value={staffFunction}
+                        />
+                    )}
+
+                    {selectedRole === 'STUDENT' && (
+                        <TextInput onChange={setDiplome} title={t('signup_page.diplome_title')} value={diplome} />
+                    )}
+
+                    {!selectedRole && <p className={styles.information}>{t('signup_page.footer')}</p>}
+                    <button className="primary-button large-margin-bottom" onClick={() => null}>
+                        {t('signup_page.validate_button')}
                     </button>
                 </div>
             </IonContent>
