e73474553c5369d2c410ab7c65a9cead81da2b48
Marvyn <marvyn.orourke@thetribe.io>
2023-07-18 10:57:28 +0200

    feat: Create new component for success pages

---
+++ app/src/presentation/pages/SignupFinalPage.tsx
@@ -0,0 +1,11 @@
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router';
+
+const SignupFinalPage: React.FC = () => {
+    const { t } = useTranslation();
+    const history = useHistory();
+
+    return <div />;
+};
+
+export default SignupFinalPage;
