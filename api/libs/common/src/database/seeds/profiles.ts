0b192245fca0dcdb6aa2a6e7d1d5522b2410066d
michael.weydert <michael.weydert@thetribe.io>
2023-08-09 11:31:04 +0200

    chore: improve profile seeds to have more matching profiles

--- api/libs/common/src/database/seeds/profiles.ts
+++ api/libs/common/src/database/seeds/profiles.ts
@@ -3,23 +3,13 @@ import * as Prisma from '@prisma/client';
 import { ProfileFactory } from '../factories';
 import { ProficiencyLevel } from '../../../../../src/core/models';
 
-const mapCountryCodeToLanguageCode = (countryCode: string): string => {
-  switch (countryCode) {
-    case 'DE':
-      return 'de';
-    case 'FR':
-      return 'fr';
-    case 'CN':
-      return 'zh';
-    default:
-      return 'en';
-  }
-};
-
 const enumValue = <T>(_enum: unknown): T => {
   const entries = Object.entries(_enum).map(([, v]) => v.toUpperCase());
   return faker.helpers.arrayElement(entries) as T;
 };
+
+const FRENCH_LANGUAGE_CODE = 'fr';
+const ENGLISH_LANGUAGE_CODE = 'en';
 
 export const createProfiles = async (
   count: number,
@@ -33,20 +23,54 @@ export const createProfiles = async (
     },
     take: count,
   });
-  const countries = await prisma.countryCodes.findMany();
+  const languages = await prisma.languageCodes.findMany();
   const objectives = await prisma.learningObjectives.findMany();
   const interest = await prisma.interests.findMany();
 
-  for (const user of users) {
+  for (const [index, user] of users.entries()) {
     const instance = profileFactory.makeOne();
 
-    const nativeLanguageCode = mapCountryCodeToLanguageCode(
-      faker.helpers.arrayElement(countries).code,
-    );
-
+    let nativeLanguageCode = faker.helpers.arrayElement(languages).code;
     const availableLanguages = user.Organization.Languages.filter(
-      (language) => language.code !== nativeLanguageCode,
+      (language) => language.code !== nativeLanguageCode.toLowerCase(),
     );
+    let learningLanguages = faker.helpers
+      .arrayElements(availableLanguages)
+      .map((learningLanguage) => ({
+        language: learningLanguage,
+        level: enumValue(ProficiencyLevel),
+      }));
+
+    if (index % 10 === 0) {
+      // Force one of 10 learning french
+      const frenchLanguage = availableLanguages.find(
+        (language) => language.code === FRENCH_LANGUAGE_CODE,
+      );
+      learningLanguages = [
+        {
+          language: frenchLanguage,
+          level: enumValue(ProficiencyLevel),
+        },
+      ];
+    } else if (index % 4 === 0) {
+      // Force on of 4 to be french
+      nativeLanguageCode = FRENCH_LANGUAGE_CODE;
+      learningLanguages = learningLanguages.filter(
+        (learningLanguage) =>
+          learningLanguage.language.code !== FRENCH_LANGUAGE_CODE,
+      );
+      if (learningLanguages.length === 0) {
+        const englishLanguage = availableLanguages.find(
+          (language) => language.code === ENGLISH_LANGUAGE_CODE,
+        );
+        learningLanguages = [
+          {
+            language: englishLanguage,
+            level: enumValue(ProficiencyLevel),
+          },
+        ];
+      }
+    }
 
     await prisma.profiles.create({
       data: {
@@ -64,16 +88,14 @@ export const createProfiles = async (
           })),
         },
         LearningLanguages: {
-          create: faker.helpers
-            .arrayElements(availableLanguages)
-            .map((learningLanguage) => {
-              return {
-                LanguageCode: {
-                  connect: { code: learningLanguage.code },
-                },
-                level: enumValue(ProficiencyLevel),
-              };
-            }),
+          create: learningLanguages.map((learningLanguage) => {
+            return {
+              LanguageCode: {
+                connect: { code: learningLanguage.language.code },
+              },
+              level: learningLanguage.level as ProficiencyLevel,
+            };
+          }),
         },
         learning_type: instance.learningType,
         same_gender: instance.sameGender,
