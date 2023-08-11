a757f230988b90cac1c12d74da4ca6b11d9d014a
Marvyn <marvyn.orourke@thetribe.io>
2023-08-11 15:08:25 +0200

    ✨ feat: handle refresh token and handle access token expiration

--- admin/src/providers/authProvider.ts
+++ admin/src/providers/authProvider.ts
@@ -37,25 +37,54 @@ const authProvider = () => ({
         );
 
         const payload = await response.json();
-        jwtManager.setToken(payload.access_token);
+        jwtManager.setTokens(payload.access_token, payload.refresh_token);
     },
     logout: () => {
-        jwtManager.ereaseToken();
+        jwtManager.ereaseTokens();
 
         return Promise.resolve();
     },
-    checkAuth: () => (jwtManager.getToken() ? Promise.resolve() : Promise.reject()),
+    checkAuth: async () => {
+        if (jwtManager.getToken('access_token')) {
+            return Promise.resolve();
+        }
+        const refreshToken = jwtManager.getToken('refresh_token');
+        if (!refreshToken) {
+            jwtManager.ereaseTokens();
+
+            return Promise.reject();
+        }
+
+        const response = await http(
+            'POST',
+            `${KEYCLOAK_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token`,
+            {
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: new URLSearchParams({
+                    grant_type: 'refresh_token',
+                    client_id: KEYCLOAK_CLIENT_ID!,
+                    client_secret: KEYCLOAK_CLIENT_SECRET!,
+                    refresh_token: refreshToken,
+                }),
+            },
+        );
+
+        const payload = await response.json();
+        jwtManager.setTokens(payload.access_token, payload.refresh_token);
+
+        return Promise.resolve();
+    },
     checkError: async (error: any) => {
         const { status } = error;
         if (status === 401 || status === 403) {
-            jwtManager.ereaseToken();
+            jwtManager.ereaseTokens();
 
             return Promise.reject();
         }
 
         return Promise.resolve();
     },
-    getPermissions: () => (jwtManager.getToken() ? Promise.resolve() : Promise.reject()),
+    getPermissions: () => (jwtManager.getToken('access_token') ? Promise.resolve() : Promise.reject()),
 });
 
 export default authProvider;
