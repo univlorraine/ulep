03dd6b2938da3e361460104ba770a2a693ece53d
michael.weydert <michael.weydert@thetribe.io>
2023-09-18 12:05:58 +0200

    doc(api): array must contains 2 items

--- api/src/api/dtos/tandems/create-tandem.request.ts
+++ api/src/api/dtos/tandems/create-tandem.request.ts
@@ -2,7 +2,7 @@ import * as Swagger from '@nestjs/swagger';
 import { ArrayNotEmpty } from 'class-validator';
 
 export class CreateTandemRequest {
-  @Swagger.ApiProperty({ type: 'string', isArray: true })
+  @Swagger.ApiProperty({ type: 'string', isArray: true, minItems: 2 })
   @ArrayNotEmpty()
   learningLanguageIds: string[];
 }
--- api/src/api/dtos/tandems/refuse-tandem.request.ts
+++ api/src/api/dtos/tandems/refuse-tandem.request.ts
@@ -2,7 +2,7 @@ import * as Swagger from '@nestjs/swagger';
 import { ArrayNotEmpty } from 'class-validator';
 
 export class RefuseTandemRequest {
-  @Swagger.ApiProperty({ type: 'string', isArray: true, minLength: 2 })
+  @Swagger.ApiProperty({ type: 'string', isArray: true, minItems: 2 })
   @ArrayNotEmpty()
   learningLanguageIds: string[];
 }
