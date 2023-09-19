9854dc6f9743bf9d3625d63a408979880827df76
michael.weydert <michael.weydert@thetribe.io>
2023-09-19 11:15:30 +0200

    feat(api): smtp gateway

---
+++ api/src/providers/gateway/smtp-email.gateway.ts
@@ -0,0 +1,52 @@
+import { EmailGateway, SendEmailPayload } from 'src/core/ports/email.gateway';
+import { Transporter, createTransport } from 'nodemailer';
+import { configuration } from 'src/configuration';
+import { Logger } from '@nestjs/common';
+
+export class SmtpEmailGateway implements EmailGateway {
+  private readonly logger = new Logger(SmtpEmailGateway.name);
+
+  #transporter: Transporter;
+  #from: string;
+
+  constructor() {
+    const { smtp } = configuration();
+
+    this.#transporter = createTransport({
+      host: smtp.host,
+      port: smtp.port,
+      secure: smtp.secure,
+    });
+    this.#from = smtp.sender;
+
+    this.logger.log(
+      `Smtp email gateway setup with transport ${smtp.host}:${smtp.port}; secure: ${smtp.secure}; from: ${smtp.sender}`,
+    );
+  }
+
+  send({ recipient, subject, content }: SendEmailPayload): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.#transporter.sendMail(
+        {
+          from: this.#from,
+          to: recipient,
+          subject,
+          text: content,
+          html: `<html><p>${content}</p></html>`,
+        },
+        (err: Error) => {
+          if (err) {
+            this.logger.error(err);
+            return reject(err);
+          }
+
+          return resolve();
+        },
+      );
+    });
+  }
+
+  async bulkSend(payloads: SendEmailPayload[]): Promise<void> {
+    await Promise.all(payloads.map((payload) => this.send(payload)));
+  }
+}
