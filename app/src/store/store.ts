85acaab1f1375492ca6856b033812c759b5347b3
Marvyn <marvyn.orourke@thetribe.io>
2023-09-22 18:40:12 +0200

    👌 fix: Add loading flag to avoir redirection + remove profileSignUp after asking learningLanguage

--- app/src/store/store.ts
+++ app/src/store/store.ts
@@ -34,8 +34,10 @@ const Store = createStore<TokenStoreTypes>(
             updateProfile: action((state, payload) => {
                 const profile = state.profile;
                 if (profile && payload.avatar) profile.user.avatar = payload.avatar;
-                if (profile && payload.learningLanguage)
+                if (profile && payload.learningLanguage) {
+                    state.profileSignUp = new ProfileSignUp();
                     profile.learningLanguages = [...profile.learningLanguages, payload.learningLanguage];
+                }
             }),
             updateProfileSignUp: action((state, payload) => {
                 const profile = state.profileSignUp;
