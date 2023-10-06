ebcccb618373aa344ceab400ffb3f09266088aa1
michael.weydert <michael.weydert@thetribe.io>
2023-10-06 17:10:27 +0200

    feat(app): improve auth page flow

--- app/src/presentation/pages/AuthFlowPage.tsx
+++ app/src/presentation/pages/AuthFlowPage.tsx
@@ -1,24 +1,30 @@
 import { IonPage } from "@ionic/react";
-import { useLocation } from "react-router";
+import { useHistory, useLocation } from "react-router";
 import { useEffect } from "react";
 import { useConfig } from "../../context/ConfigurationContext";
+import { TailSpin } from "react-loader-spinner";
+import CenterLayout from "../components/layout/CenterLayout";
 
-const AuthPage: React.FC = () => {    
+const AuthPage: React.FC = () => {
     const location = useLocation();
     const params = new URLSearchParams(location.search)
     const code = params.get("code");
     const { getTokenFromCodeUsecase } = useConfig();
+    const history = useHistory();
+    const { configuration } = useConfig();
 
     useEffect(() => {
         if (code) {
             getTokenFromCodeUsecase.execute(code).then((res) => {
                 if ("accessToken" in res) {
-                    console.log("SUCCESS, should redirect");
-                    // TODO(NOW): redirect
+                    // TODO(future): call connector here to initialize store with values from university ?
+                    history.push("/signup/informations");
                 }
             }).catch((err) => {
                 console.error(err);
-                // TODO(NOW): redirect error page
+                window.alert("an error occured");
+                // TODO: redirect on error page when exist
+                history.push("/error");
             });
         }
     }, [code]);
@@ -26,10 +32,18 @@ const AuthPage: React.FC = () => {
 
     return (
         <IonPage>
-            <div style={{color: "red"}}>
-                {/* TODO(NOW): real loader */}
-                Loading auth
-            </div>
+            <CenterLayout>
+                <TailSpin
+                        height="150"
+                        width="150"
+                        color={configuration.primaryColor}
+                        ariaLabel="tail-spin-loading"
+                        radius="1"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                        visible={true}
+                    />
+            </CenterLayout>
         </IonPage>
     );
 };
