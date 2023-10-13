ea01ed02183503101856525c125fd0062f806532
Marvyn <marvyn.orourke@thetribe.io>
2023-10-13 14:56:00 +0200

    ✨ feat: Set tandems to inactive for user banned

--- api/src/core/usecases/user/update-user.usecase.ts
+++ api/src/core/usecases/user/update-user.usecase.ts
@@ -2,6 +2,10 @@ import { Inject, Injectable } from '@nestjs/common';
 import { USER_REPOSITORY, UserRepository } from '../../ports/user.repository';
 import { RessourceDoesNotExist } from 'src/core/errors';
 import { User, UserStatus } from 'src/core/models';
+import {
+  TANDEM_REPOSITORY,
+  TandemRepository,
+} from 'src/core/ports/tandems.repository';
 
 export class UpdateUserCommand {
   id: string;
@@ -14,12 +18,21 @@ export class UpdateUserUsecase {
   constructor(
     @Inject(USER_REPOSITORY)
     private readonly userRepository: UserRepository,
+    @Inject(TANDEM_REPOSITORY)
+    private readonly tandemRepository: TandemRepository,
   ) {}
 
   async execute(command: UpdateUserCommand) {
     const user = await this.userRepository.ofId(command.id);
     if (!user) {
       throw new RessourceDoesNotExist();
+    }
+
+    if (
+      user.status !== UserStatus.BANNED &&
+      command.status === UserStatus.BANNED
+    ) {
+      await this.tandemRepository.disableTandemsForUser(user.id);
     }
 
     return this.userRepository.update(
