e60848722061223a20349db665c57317ab534d46
Marvyn <marvyn.orourke@thetribe.io>
2023-11-08 16:15:32 +0100

    Add version number to Sentry release in back office

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -2,6 +2,7 @@ import * as Sentry from '@sentry/react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { version } from '../package.json';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -17,7 +18,7 @@ Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
     debug: process.env.REACT_APP_ENV === 'dev',
     integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
-    release: `ulep-admin@${process.env.npm_package_version}`,
+    release: `ulep-admin@${version}`,
     dist: '1',
     environment: process.env.REACT_APP_ENV,
     tracesSampleRate: 1.0,
