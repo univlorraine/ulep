30cab09d47ffae2d2e3cf6fd6e3b7ad801baaa07
herveGuigoz <herve.guigoz@gmail.com>
2024-01-10 14:57:42 +0100

    fix: 3474 fix age bonus computation in match score

--- api/src/core/services/MatchScorer.ts
+++ api/src/core/services/MatchScorer.ts
@@ -14,6 +14,11 @@ export type Coeficients = {
   certificateOption: number;
 };
 
+export type AgeThreshold = {
+  age: number;
+  maxDifference: number;
+}[];
+
 export interface IMatchScorer {
   computeMatchScore(
     learningLanguage1: LearningLanguage,
@@ -24,7 +29,7 @@ export interface IMatchScorer {
 
 const getMaxValueInNumberMatrix = (matrix: { [key: string]: { [key: string]: number } }) => Object.values(matrix)
   .reduce<number>((accumulator, value) => {
-    const maxScoreForValue = Math.max(...Object.values(value)); 
+    const maxScoreForValue = Math.max(...Object.values(value));
     if (!accumulator || maxScoreForValue > accumulator) {
       return maxScoreForValue
     }
@@ -35,17 +40,26 @@ const getMaxValueInNumberMatrix = (matrix: { [key: string]: { [key: string]: num
 
 @Injectable()
 export class MatchScorer implements IMatchScorer {
-
+  // Coeficients used to compute the match score
   #coeficients: Coeficients = {
-    level: 0.70,
-    age: 0.05,
-    status: 0.05,
+    level: 0.60,
+    age: 0.10,
+    status: 0.10,
     goals: 0.05,
     interests: 0.05,
     meetingFrequency: 0.05,
     certificateOption: 0.05,
   };
 
+  // Structure for age thresholds
+  #ageThresholds: AgeThreshold = [
+    { age: 50, maxDifference: 10 },
+    { age: 40, maxDifference: 10 },
+    { age: 30, maxDifference: 10 },
+    { age: 25, maxDifference: 5 },
+    { age: 20, maxDifference: 5 },
+    { age: 15, maxDifference: 5 },
+  ];
 
   // Note: A learning language can only match a language spoken by the potential match profile. As all
   // languages spoken are approximated with the same skill level ,we approximate the compatibility 
@@ -78,6 +92,7 @@ export class MatchScorer implements IMatchScorer {
   constructor() {
     this.#standardPairingLearningLanguagesCompatibilityMatrixMaxScore = getMaxValueInNumberMatrix(this.#standardPairingLearningLanguagesCompatibilityMatrix);
     this.#discoveryPairingLearningLanguagesCompatibilityMatrixMaxScore = getMaxValueInNumberMatrix(this.#discoveryPairingLearningLanguagesCompatibilityMatrix);
+    this.#ageThresholds.sort((a, b) => b.age - a.age);
   }
 
   public set coeficients(coeficients: Coeficients) {
@@ -112,10 +127,13 @@ export class MatchScorer implements IMatchScorer {
     )) {
       return new Match({ owner: learningLanguage1, target: learningLanguage2, scores: MatchScores.empty() });
     }
+    
+    // Check if age bonus should be applied (i.e. if one of the two profiles is looking for a partner of the same age)
+    const shouldApplyAgeBonus = learningLanguage1.sameAge || learningLanguage2.sameAge;
 
     const scores: MatchScores = new MatchScores({
       level: this.computeLearningCompatibility(learningLanguage1, learningLanguage2),
-      age: this.computeAgeBonus(profile1, profile2),
+      age: shouldApplyAgeBonus ? this.computeAgeBonus(profile1, profile2) : 0,
       status: this.computeSameRolesBonus(profile1, profile2),
       goals: this.computeSameGoalsBonus(profile1, profile2),
       interests: this.computeSameInterestBonus(profile1, profile2),
@@ -138,7 +156,7 @@ export class MatchScorer implements IMatchScorer {
    */
   private computeLearningCompatibility(learningLanguage1: LearningLanguage, learningLanguage2: LearningLanguage): number {
     const isDiscovery = learningLanguage1.isDiscovery(learningLanguage2) || learningLanguage2.isDiscovery(learningLanguage1);
-  
+
     const score = isDiscovery
       ? this.#discoveryPairingLearningLanguagesCompatibilityMatrix[learningLanguage1.level][learningLanguage2.level] / this.#discoveryPairingLearningLanguagesCompatibilityMatrixMaxScore
       : this.#standardPairingLearningLanguagesCompatibilityMatrix[learningLanguage1.level][learningLanguage2.level] / this.#standardPairingLearningLanguagesCompatibilityMatrixMaxScore;
@@ -150,33 +168,23 @@ export class MatchScorer implements IMatchScorer {
   private computeAgeBonus(profile1: Profile, profile2: Profile): number {
     // Compute the absolute age difference between the two profiles
     const ageDiff: number = Math.abs(profile1.user.age - profile2.user.age);
+    
+    for (const threshold of this.#ageThresholds) {
+      // Check if at least one of the profiles is equal to or older than the threshold age
+      if (profile1.user.age >= threshold.age || profile2.user.age >= threshold.age) {
+        // Calculate the differenceFactorPercent based on ageBonus and maxDifference
+        const differenceFactorPercent = this.#coeficients.age / threshold.maxDifference;
+        // Calculate the differenceFactor
+        const differenceFactor = differenceFactorPercent * ageDiff;
+        // Calculate the ponderation
+        const ponderation = this.#coeficients.age - differenceFactor;
 
-    // If age of one of profile is greater than 50
-    if (profile1.user.age > 50 || profile2.user.age > 50) {
-      // Apply the age bonus if the age of the second profile is greater than 45 or vice versa
-      // If the other profile is 45 or younger, return the original score
-      if (
-        (profile1.user.age > 50 && profile2.user.age > 45)
-        || (profile2.user.age > 50 && profile1.user.age > 45)
-      ) {
-        return this.coeficients.age;  
-      } else {
-        return 0;
+        return ponderation > 0 ? ponderation : 0;
       }
     }
 
-    // If the age of the first profile is greater than 30 (but not greater than 50,
-    // because of the previous condition)
-    if (profile1.user.age > 30 || profile2.user.age > 30) {
-      // Apply the age bonus if the age difference between the profiles is inferior or equal to 10
-      // If the age difference is outside this range, return the original score
-      return ageDiff <= 10 ? this.coeficients.age : 0;
-    }
-
-    // If the age of the first profile is 30 or younger
-    // Apply the age bonus if the age difference between the profiles is inferior or equal to 3
-    // If the age difference is outside this range, return the original score
-    return ageDiff <= 3 ? this.coeficients.age : 0;
+    // If none of the age thresholds are applicable, return 0 as no bonus is applicable.
+    return 0;
   }
 
   // Apply bonus if profiles share the same status
@@ -224,7 +232,8 @@ export class MatchScorer implements IMatchScorer {
 
     return intersection.size / union.size;
   }
-
+  
+  // Apply bonus if profiles share the same meeting frequency
   private computeMeetingFrequencyBonus(
     profile1: Profile,
     profile2: Profile
@@ -236,6 +245,7 @@ export class MatchScorer implements IMatchScorer {
     return 0;
   }
 
+  // Apply bonus if profiles share the same certificate option
   private computeCertificateOptionBonus(
     learningLanguage1: LearningLanguage,
     learningLanguage2: LearningLanguage
@@ -243,7 +253,7 @@ export class MatchScorer implements IMatchScorer {
     if (!!learningLanguage1.certificateOption === !!learningLanguage2.certificateOption) {
       return this.#coeficients.certificateOption;
     }
-    
+
     return 0;
   }
 
@@ -254,7 +264,7 @@ export class MatchScorer implements IMatchScorer {
   ): boolean {
     const profile1 = learningLanguage1.profile;
     const profile2 = learningLanguage2.profile;
-    
+
     // Check joker language have a match in available languages spoken by other profile
     if (learningLanguage1.language.isJokerLanguage()) {
       if (!profile1.canLearnALanguageFromProfile(profile2, availableLanguages)) {
@@ -277,14 +287,14 @@ export class MatchScorer implements IMatchScorer {
     if ((learningLanguage1.sameGender || learningLanguage2.sameGender)
       && profile1.user.gender !== profile2.user.gender
     ) {
-        return false;
+      return false;
     }
 
     // Check incompatibilities between learning types
     if (learningLanguage1.learningType !== learningLanguage2.learningType && (
       learningLanguage1.learningType !== LearningType.BOTH && learningLanguage2.learningType !== LearningType.BOTH
     )) {
-        return false;
+      return false;
     }
 
     // Check same campus if tandem
@@ -295,8 +305,8 @@ export class MatchScorer implements IMatchScorer {
         (!learningLanguage1.campus || !learningLanguage2.campus)
         || (learningLanguage1.campus.id !== learningLanguage2.campus.id)
       )
-     ) {
-        return false;
+    ) {
+      return false;
     }
 
     return true;
