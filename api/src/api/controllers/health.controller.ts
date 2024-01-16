2defa50c7f5f55a8b632c71d2c229c8cc63be30c
herveGuigoz <herve.guigoz@gmail.com>
2024-01-16 01:04:52 +0100

    fix(api): health endpoint

--- api/src/api/controllers/health.controller.ts
+++ api/src/api/controllers/health.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { HealthCheckService } from '@nestjs/terminus';
+import { Controller, Get, SerializeOptions } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
 import * as Swagger from '@nestjs/swagger';
+import { PrismaService } from '@app/common';
 
 @Controller('health')
 @Swagger.ApiExcludeController()
 export class HealthController {
-  constructor(private healthCheckService: HealthCheckService) {}
+  constructor(
+    private readonly healthCheckService: HealthCheckService,
+    private readonly prisma: PrismaHealthIndicator,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
+  @HealthCheck()
+  @SerializeOptions({ strategy: 'exposeAll' })
   async health(): Promise<any> {
-    return this.healthCheckService.check([]);
+    return this.healthCheckService.check([
+      () => this.prisma.pingCheck('prisma', this.prismaService),
+    ]);
   }
 }
