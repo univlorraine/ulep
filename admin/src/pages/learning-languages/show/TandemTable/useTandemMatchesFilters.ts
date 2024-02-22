9e90d7eb4fa8a989ae1b8c44ea6b679e00e69b87
michael.weydert <michael.weydert@thetribe.io>
2024-02-22 10:29:10 +0100

    perf: optimize front filtering for Tandem matches

--- admin/src/pages/learning-languages/show/TandemTable/useTandemMatchesFilters.ts
+++ admin/src/pages/learning-languages/show/TandemTable/useTandemMatchesFilters.ts
@@ -5,21 +5,29 @@ const useTandemMatchesFilters = (matches: Match[]) => {
     const [firstnameFilter, setFirstnameFilter] = useState<string>();
     const [lastnameFilter, setLastnameFilter] = useState<string>();
     const [roleFilter, setRoleFilter] = useState<UserRole>();
-    let filteredMatches = matches || [];
-    if (firstnameFilter) {
-        filteredMatches = filteredMatches.filter((match) =>
-            match.target.profile.user.firstname.toLowerCase().includes(firstnameFilter.toLowerCase())
-        );
-    }
-    // TODO(NOW+1): optimize loop for filtering
-    if (lastnameFilter) {
-        filteredMatches = filteredMatches.filter((match) =>
-            match.target.profile.user.lastname.toLowerCase().includes(lastnameFilter.toLowerCase())
-        );
-    }
-    if (roleFilter) {
-        filteredMatches = filteredMatches.filter((match) => match.target.profile.user.role === roleFilter);
-    }
+
+    const filteredMatches =
+        firstnameFilter || lastnameFilter || roleFilter
+            ? matches.filter((match) => {
+                  if (
+                      firstnameFilter &&
+                      !match.target.profile.user.firstname.toLowerCase().includes(firstnameFilter.toLowerCase())
+                  ) {
+                      return false;
+                  }
+                  if (
+                      lastnameFilter &&
+                      !match.target.profile.user.lastname.toLowerCase().includes(lastnameFilter.toLowerCase())
+                  ) {
+                      return false;
+                  }
+                  if (roleFilter && match.target.profile.user.role !== roleFilter) {
+                      return false;
+                  }
+
+                  return true;
+              })
+            : matches;
 
     return {
         filteredMatches,
