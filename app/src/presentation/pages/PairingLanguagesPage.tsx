79fcff742876b80b540265801f89eb4db740b391
michael.weydert <michael.weydert@thetribe.io>
2024-02-23 14:35:38 +0100

    feat(app): add loader on pairing languages page

--- app/src/presentation/pages/PairingLanguagesPage.tsx
+++ app/src/presentation/pages/PairingLanguagesPage.tsx
@@ -10,6 +10,7 @@ import FlagBubble from '../components/FlagBubble';
 import WebLayoutCentered from '../components/layout/WebLayoutCentered';
 import pairingLanguagesStyles from './css/PairingLanguages.module.css';
 import styles from './css/SignUp.module.css';
+import { TailSpin } from 'react-loader-spinner';
 
 const PairingLanguagesPage: React.FC = () => {
     const { t } = useTranslation();
@@ -17,6 +18,7 @@ const PairingLanguagesPage: React.FC = () => {
     const [showToast] = useIonToast();
     const history = useHistory();
     const [languages, setLanguages] = useState<Language[]>([]);
+    const [isLoadingLanguages, setIsLoadingLanguages] = useState<boolean>(false);
     const [selectedLaguage, setSelectedLanguage] = useState<Language>();
     const updateProfileSignUp = useStoreActions((state) => state.updateProfileSignUp);
     const profile = useStoreState((state) => state.profile);
@@ -27,7 +29,9 @@ const PairingLanguagesPage: React.FC = () => {
     }
 
     const getLanguages = async () => {
+        setIsLoadingLanguages(true);
         let result = await getAllLanguages.execute(university.isCentral ? 'PRIMARY' : 'PARTNER');
+        setIsLoadingLanguages(false);
 
         if (result instanceof Error) {
             return await showToast({ message: t(result.message), duration: 1000 });
@@ -69,44 +73,59 @@ const PairingLanguagesPage: React.FC = () => {
             <div className={styles.body}>
                 <div className={pairingLanguagesStyles.content}>
                     <h1 className="title">{t('pairing_languages_page.title')}</h1>
-                    {!!languages.length && <p className="subtitle">{t('pairing_languages_page.subtitle')}</p>}
-
-                    {!languages.length && <p className="subtitle">{t('pairing_languages_page.no_languages')}</p>}
-
-                    <div className={pairingLanguagesStyles['languages-container']}>
-                        {!!languages.length &&
-                            languages.map((language) => {
-                                return (
-                                    <FlagBubble
-                                        key={language.code}
-                                        isSelected={selectedLaguage?.code === language.code}
-                                        language={language}
-                                        onPressed={setSelectedLanguage}
-                                    />
-                                );
-                            })}
-                        {university.isCentral && (
-                            <button style={{ background: 'none' }} onClick={otherLanguage}>
-                                <img alt="plus" className={pairingLanguagesStyles.image} src={PlusPng} />
-                            </button>
-                        )}
-                    </div>
-                </div>
-                <div className={`extra-large-margin-bottom`}>
-                    {!!languages.length && (
-                        <button
-                            className={`primary-button ${!selectedLaguage ? 'disabled' : ''}`}
-                            disabled={!selectedLaguage}
-                            onClick={continueSignUp}
-                        >
-                            {t('pairing_languages_page.validate_button')}
-                        </button>
-                    )}
-
-                    {!languages.length && (
-                        <button className={`primary-button`} onClick={navigateToHome}>
-                            {t('pairing_languages_page.home_button')}
-                        </button>
+                    {isLoadingLanguages ? (
+                        <div className={pairingLanguagesStyles.loader}>
+                            <TailSpin
+                                color={configuration.primaryColor}
+                                ariaLabel="tail-spin-loading"
+                                radius="1"
+                                visible={true}
+                            />
+                        </div>
+                    ) : (
+                        <>
+                            <p className="subtitle">
+                                {t(
+                                    languages.length
+                                        ? 'pairing_languages_page.subtitle'
+                                        : 'pairing_languages_page.no_languages'
+                                )}
+                            </p>
+                            <div className={pairingLanguagesStyles['languages-container']}>
+                                {!!languages.length &&
+                                    languages.map((language) => {
+                                        return (
+                                            <FlagBubble
+                                                key={language.code}
+                                                isSelected={selectedLaguage?.code === language.code}
+                                                language={language}
+                                                onPressed={setSelectedLanguage}
+                                            />
+                                        );
+                                    })}
+                                {!isLoadingLanguages && university.isCentral && (
+                                    <button style={{ background: 'none' }} onClick={otherLanguage}>
+                                        <img alt="plus" className={pairingLanguagesStyles.image} src={PlusPng} />
+                                    </button>
+                                )}
+                            </div>
+                            <div className={`extra-large-margin-bottom`}>
+                                {!!languages.length && (
+                                    <button
+                                        className={`primary-button ${!selectedLaguage ? 'disabled' : ''}`}
+                                        disabled={!selectedLaguage}
+                                        onClick={continueSignUp}
+                                    >
+                                        {t('pairing_languages_page.validate_button')}
+                                    </button>
+                                )}
+                                {!languages.length && (
+                                    <button className={`primary-button`} onClick={navigateToHome}>
+                                        {t('pairing_languages_page.home_button')}
+                                    </button>
+                                )}
+                            </div>
+                        </>
                     )}
                 </div>
             </div>
