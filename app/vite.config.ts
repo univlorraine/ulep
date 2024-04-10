ea61309c354fd2c94439c6a76521f8993f50a4c5
Nicolas Lescop <nicolas.lescop@thetribe.io>
2024-04-10 10:15:02 +0200

    feat(app): Generate sitemap.xml and robots.txt

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,16 +1,17 @@
 import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import svgr from 'vite-plugin-svgr';
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     optimizeDeps: {
         exclude: [`@capacitor/camera`],
     },
-    plugins: [react(), legacy(), svgr()],
+    plugins: [react(), legacy(), svgr(), sitemap()],
     define: {
-        'APP_VERSION': JSON.stringify(process.env.npm_package_version),
+        APP_VERSION: JSON.stringify(process.env.npm_package_version),
     },
     test: {
         globals: true,
