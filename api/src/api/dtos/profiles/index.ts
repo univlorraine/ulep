39b2eb7d6528d6de7691e1ebe55e14d021d8aef6
Marvyn <marvyn.orourke@thetribe.io>
2024-04-11 09:36:38 +0200

    👌 fix: Remove useless import

--- api/src/api/dtos/profiles/index.ts
+++ api/src/api/dtos/profiles/index.ts
@@ -2,4 +2,3 @@ export * from './create-profile.request';
 export * from './profiles-filters';
 export * from './profiles.response';
 export * from './update-profile.request';
-export * from '../tested-languages/tested-language.request';
