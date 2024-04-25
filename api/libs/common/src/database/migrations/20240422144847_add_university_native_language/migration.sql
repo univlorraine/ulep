88b210d555390afbad0730786a3f6ce519aa27c5
Marvyn <marvyn.orourke@thetribe.io>
2024-04-25 17:12:57 +0200

    ✨ feat: Add function for initial value

--- api/libs/common/src/database/migrations/20240422144847_add_university_native_language/migration.sql
+++ api/libs/common/src/database/migrations/20240422144847_add_university_native_language/migration.sql
@@ -1,11 +1,12 @@
--- AlterTable
-ALTER TABLE "organizations" ADD COLUMN "language_code_id" TEXT;
+-- Création d'une fonction pour obtenir l'ID
+CREATE OR REPLACE FUNCTION get_language_code_id()
+RETURNS TEXT AS $$
+BEGIN
+    RETURN (SELECT id FROM "language_codes" WHERE code = 'fr');
+END;
+$$ LANGUAGE plpgsql;
 
--- Update all existing organizations with a default language code to French
-UPDATE "organizations" SET ("language_code_id") = (SELECT id FROM "language_codes" WHERE code = 'fr');
-
--- AlterTable
-ALTER TABLE "organizations" ALTER COLUMN "language_code_id" SET NOT NULL;
+ALTER TABLE "organizations" ADD COLUMN "language_code_id" TEXT DEFAULT get_language_code_id() NOT NULL;
 
 -- AddForeignKey
 ALTER TABLE "organizations" ADD CONSTRAINT "organizations_language_code_id_fkey" FOREIGN KEY ("language_code_id") REFERENCES "language_codes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
