998c97876fcd823bd1ee92ab72df9148cbc795be
Nicolas Lescop <nicolas.lescop@thetribe.io>
2024-05-29 15:47:28 +0200

    feat(api): Get all translations from minio

--- api/libs/common/src/i18n/i18n.service.ts
+++ api/libs/common/src/i18n/i18n.service.ts
@@ -1,16 +1,13 @@
 import * as i18n from 'i18next';
 import * as HttpBackend from 'i18next-http-backend';
-import {
-  Inject,
-  Injectable,
-  Logger,
-  OnModuleDestroy,
-  OnModuleInit,
-} from '@nestjs/common';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 
 export const I18N_SERVICE_CONFIGURATION = 'I18N_SERVICE_CONFIGURATION';
 
 const DEFAULT_RELOAD_INTERVAL = 24 * 60 * 60 * 1000;
+
+const LANGUAGES = ['en', 'fr', 'zh', 'de', 'es'];
+const NAMESPACES = ['emails', 'translation', 'api', 'notifications'];
 
 export interface I18nServiceConfiguration {
   fallbackLanguage?: string;
@@ -29,13 +26,9 @@ export type I18nTranslation =
     };
 
 @Injectable()
-export class I18nService implements OnModuleInit, OnModuleDestroy {
+export class I18nService implements OnModuleInit {
   #logger: Logger;
   #i18n: i18n.i18n;
-  #i18nReload: NodeJS.Timer;
-
-  #knownLanguages: Set<string>;
-  #knownComponents: Set<string>;
 
   constructor(
     @Inject(I18N_SERVICE_CONFIGURATION)
@@ -46,125 +39,23 @@ export class I18nService implements OnModuleInit, OnModuleDestroy {
   }
 
   async onModuleInit() {
-    const { languages, components } =
-      await this.getAvailableLanguagesAndComponents();
-    this.#knownLanguages = languages;
-    this.#knownComponents = components;
-
-    const url = `${this.config.http.url}/translations/ulep/{{ns}}/{{lng}}/file/`;
+    const url = `${this.config.http.url}/{{lng}}/{{ns}}.json`;
 
     this.#i18n = i18n.use(HttpBackend as unknown as i18n.Module);
+
+    console.log({ url });
     await this.#i18n.init({
       fallbackLng: this.config.fallbackLanguage || 'en',
-      ns: [...components],
-      debug: this.config.debug,
-      preload: [...languages],
+      ns: NAMESPACES,
+      debug: true, //this.config.debug,
+      preload: LANGUAGES,
       backend: {
         loadPath: url,
         crossDomain: true,
-        withCredentials: Boolean(this.config.http.token),
-        customHeaders: () => {
-          return this.config.http.token
-            ? {
-                Authorization: `Token ${this.config.http.token}`,
-              }
-            : {};
-        },
         reloadInterval:
           this.config.http.reloadInterval || DEFAULT_RELOAD_INTERVAL,
       },
     });
-
-    if (!this.#i18nReload) {
-      this.#i18nReload = setInterval(
-        () => this.loadNewLanguagesAndComponents,
-        this.config.http.reloadInterval || DEFAULT_RELOAD_INTERVAL,
-      );
-    }
-  }
-
-  onModuleDestroy() {
-    if (this.#i18nReload) {
-      clearInterval(this.#i18nReload);
-    }
-  }
-
-  private async getAvailableLanguagesAndComponents(): Promise<{
-    languages: Set<string>;
-    components: Set<string>;
-  }> {
-    const translationsUrl = `${this.config.http.url}/translations/`;
-
-    const res = await fetch(translationsUrl, {
-      headers: {
-        Authorization: `Token ${this.config.http.token}`,
-      },
-    });
-    if (!res.ok) {
-      try {
-        const data = await res.json();
-        this.#logger.error(data);
-      } catch (err) {
-        this.#logger.error(res.body);
-      }
-      return {
-        components: new Set(),
-        languages: new Set(),
-      };
-    } else {
-      const data = await res.json();
-      return data.results.reduce(
-        (acc, value) => {
-          const code = value.language_code;
-          const component = value.component.slug;
-
-          // Glossary are components created by Weblate
-          if (component !== 'glossary') {
-            if (acc.languages.has(code) <= 0) {
-              acc.languages.add(code);
-            }
-            if (acc.components.has(component) <= 0) {
-              acc.components.add(component);
-            }
-          }
-
-          return acc;
-        },
-        {
-          components: new Set(),
-          languages: new Set(),
-        },
-      );
-    }
-  }
-
-  private async loadNewLanguagesAndComponents() {
-    const { languages: updatedLanguages, components: updatedComponents } =
-      await this.getAvailableLanguagesAndComponents();
-
-    const newLanguages = [...updatedLanguages].filter(
-      (language) => !this.#knownLanguages.has(language),
-    );
-    if (newLanguages.length > 0) {
-      this.#logger.debug(`Languages to add: ${newLanguages.join(', ')}`);
-      await this.#i18n.loadLanguages(newLanguages);
-      for (const language of newLanguages) {
-        this.#knownLanguages.add(language);
-      }
-      this.#logger.log(`Added languages: ${newLanguages.join(', ')}`);
-    }
-
-    const newComponents = [...updatedComponents].filter(
-      (component) => !this.#knownComponents.has(component),
-    );
-    if (newComponents.length > 0) {
-      this.#logger.debug(`Namespace to add: ${newComponents.join(', ')}`);
-      await this.#i18n.loadNamespaces(newComponents);
-      for (const component of newComponents) {
-        this.#knownComponents.add(component);
-      }
-      this.#logger.log(`Added namespace: ${newComponents.join(', ')}`);
-    }
   }
 
   public translate(key: string, opts: any) {
@@ -174,6 +65,7 @@ export class I18nService implements OnModuleInit, OnModuleDestroy {
   public hasLanguageBundle(language: string, namespace: string) {
     return this.#i18n.hasResourceBundle(language, namespace);
   }
+
   public getLanguageBundle(language: string, namespace: string) {
     return this.#i18n.getResourceBundle(language, namespace);
   }
