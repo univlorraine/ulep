db1bb93568c9e0993a1bf784369ce05c68b8b4cd
Marvyn <marvyn.orourke@thetribe.io>
2024-07-03 15:36:48 +0200

    👌 fix: Remove IonPage on HomePage

--- app/src/presentation/components/contents/HomeContent.tsx
+++ app/src/presentation/components/contents/HomeContent.tsx
@@ -1,21 +1,21 @@
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent } from '@ionic/react';
 import { useTranslation } from 'react-i18next';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import { useHistory } from 'react-router';
 import { ArrowDownSvg, ReportSvg } from '../../../assets';
+import Profile from '../../../domain/entities/Profile';
 import Tandem from '../../../domain/entities/Tandem';
-import TandemList from '../../components/tandems/TandemList';
-import WaitingTandemList from '../../components/tandems/WaitingTandemList';
-import { HYBRID_MAX_WIDTH, learningLanguagesToTestedLanguages } from '../../utils';
-import styles from './HomeContent.module.css';
+import University from '../../../domain/entities/University';
 import Avatar from '../../components/Avatar';
 import Loader from '../../components/Loader';
 import MyUniversityCard from '../../components/card/MyUniversityCard';
 import PartnerUniversitiesCard from '../../components/card/PartnerUniversitiesCard';
 import ProficiencyTestCard from '../../components/card/ProficiencyTestCard';
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
-import Profile from '../../../domain/entities/Profile';
+import TandemList from '../../components/tandems/TandemList';
+import WaitingTandemList from '../../components/tandems/WaitingTandemList';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
-import University from '../../../domain/entities/University';
+import { HYBRID_MAX_WIDTH, learningLanguagesToTestedLanguages } from '../../utils';
+import styles from './HomeContent.module.css';
 
 interface HomeContentProps {
     isLoading: boolean;
@@ -51,76 +51,74 @@ const HomeContent: React.FC<HomeContentProps> = ({
         .padStart(2, '0')}`;
 
     return (
-        <IonPage className={styles.content}>
-            <IonContent>
-                <div className={`${styles.container} content-wrapper`}>
-                    <div className={styles['header']}>
-                        <div className={styles['hello-container']}>
-                            <span className={styles.date}>{formattedDate}</span>
-                            <span className={styles.hello}>{`${t('global.hello')} ${profile.user.firstname}`}</span>
-                        </div>
-                        {isHybrid && (
-                            <button
-                                aria-label={t('global.change_avatar') as string}
-                                className={styles['avatar-container']}
-                                onClick={onProfilePressed}
-                            >
-                                <Avatar user={profile.user} className={styles.avatar} />
-                                <img alt="" src={ArrowDownSvg} />
-                            </button>
-                        )}
+        <IonContent className={styles.content}>
+            <div className={`${styles.container} content-wrapper`}>
+                <div className={styles['header']}>
+                    <div className={styles['hello-container']}>
+                        <span className={styles.date}>{formattedDate}</span>
+                        <span className={styles.hello}>{`${t('global.hello')} ${profile.user.firstname}`}</span>
                     </div>
-                    {isHybrid && <div className={styles.separator} />}
-                    <div className={styles['masonery-content']}>
-                        {isLoading ? (
-                            <div className={styles.loaderContainer}>
-                                <Loader />
-                            </div>
-                        ) : (
-                            <ResponsiveMasonry columnsCountBreakPoints={{ 300: 1, 768: 2 }}>
-                                <Masonry className={styles.masonery} gutter="20px">
-                                    {tandems.find((tandem) => tandem.status === 'ACTIVE') && (
-                                        <TandemList onTandemPressed={onValidatedTandemPressed} tandems={tandems} />
-                                    )}
-                                    <WaitingTandemList
-                                        onTandemPressed={onTandemPressed}
-                                        onNewTandemAsked={() => history.push('pairing/languages')}
-                                        profile={profile}
-                                        tandems={tandems}
-                                    />
-                                    <MyUniversityCard university={profile.user.university} />
-                                    {partnerUniversities?.length > 0 && (
-                                        <PartnerUniversitiesCard universities={partnerUniversities} />
-                                    )}
-                                    {(profile.learningLanguages.length > 0 || profile.testedLanguages.length > 0) && (
-                                        <ProficiencyTestCard
-                                            testedLanguages={learningLanguagesToTestedLanguages(
-                                                profile.learningLanguages,
-                                                profile.testedLanguages
-                                            )}
-                                        />
-                                    )}
-                                </Masonry>
-                            </ResponsiveMasonry>
-                        )}
-                    </div>
-                    <div className={styles['report-container']}>
+                    {isHybrid && (
                         <button
-                            aria-label={t('home_page.report.report_button') as string}
-                            className={`tertiary-button ${styles.report}`}
-                            onClick={onReportPressed}
+                            aria-label={t('global.change_avatar') as string}
+                            className={styles['avatar-container']}
+                            onClick={onProfilePressed}
                         >
-                            {
-                                <>
-                                    <img alt="" className="margin-right" src={ReportSvg} />
-                                    <span>{t('home_page.report.report_button')}</span>
-                                </>
-                            }
+                            <Avatar user={profile.user} className={styles.avatar} />
+                            <img alt="" src={ArrowDownSvg} />
                         </button>
-                    </div>
+                    )}
                 </div>
-            </IonContent>
-        </IonPage>
+                {isHybrid && <div className={styles.separator} />}
+                <div className={styles['masonery-content']}>
+                    {isLoading ? (
+                        <div className={styles.loaderContainer}>
+                            <Loader />
+                        </div>
+                    ) : (
+                        <ResponsiveMasonry columnsCountBreakPoints={{ 300: 1, 768: 2 }}>
+                            <Masonry className={styles.masonery} gutter="20px">
+                                {tandems.find((tandem) => tandem.status === 'ACTIVE') && (
+                                    <TandemList onTandemPressed={onValidatedTandemPressed} tandems={tandems} />
+                                )}
+                                <WaitingTandemList
+                                    onTandemPressed={onTandemPressed}
+                                    onNewTandemAsked={() => history.push('pairing/languages')}
+                                    profile={profile}
+                                    tandems={tandems}
+                                />
+                                <MyUniversityCard university={profile.user.university} />
+                                {partnerUniversities?.length > 0 && (
+                                    <PartnerUniversitiesCard universities={partnerUniversities} />
+                                )}
+                                {(profile.learningLanguages.length > 0 || profile.testedLanguages.length > 0) && (
+                                    <ProficiencyTestCard
+                                        testedLanguages={learningLanguagesToTestedLanguages(
+                                            profile.learningLanguages,
+                                            profile.testedLanguages
+                                        )}
+                                    />
+                                )}
+                            </Masonry>
+                        </ResponsiveMasonry>
+                    )}
+                </div>
+                <div className={styles['report-container']}>
+                    <button
+                        aria-label={t('home_page.report.report_button') as string}
+                        className={`tertiary-button ${styles.report}`}
+                        onClick={onReportPressed}
+                    >
+                        {
+                            <>
+                                <img alt="" className="margin-right" src={ReportSvg} />
+                                <span>{t('home_page.report.report_button')}</span>
+                            </>
+                        }
+                    </button>
+                </div>
+            </div>
+        </IonContent>
     );
 };
 
