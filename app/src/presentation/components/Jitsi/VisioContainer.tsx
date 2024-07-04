2c6fcd4a9ac7eef7aa5f3adc6576a8be1270ceb6
Marvyn <marvyn.orourke@thetribe.io>
2024-07-04 11:18:08 +0200

    👌 fix: Move to home when getJitsiToken failed

--- app/src/presentation/components/Jitsi/VisioContainer.tsx
+++ app/src/presentation/components/Jitsi/VisioContainer.tsx
@@ -1,11 +1,10 @@
+import { IonLoading, isPlatform } from '@ionic/react';
 import { useEffect, useState } from 'react';
-import { isPlatform, IonLoading } from '@ionic/react';
-import { useLocation } from 'react-router-dom';
-import { useConfig } from '../../../context/ConfigurationContext';
 import { useTranslation } from 'react-i18next';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useConfig } from '../../../context/ConfigurationContext';
 import JitsiMobile from './JitsiMobile';
 import JitsiWeb from './JitsiWeb';
-import useLogout from '../../hooks/useLogout';
 
 export interface JitsiProps {
     jitsiDomain: string;
@@ -18,7 +17,7 @@ const VisioContainer = () => {
     const { i18n } = useTranslation();
     const location = useLocation();
     const { accessToken, getJitsiToken } = useConfig();
-    const { handleLogout } = useLogout({ forceRedirect: true });
+    const history = useHistory();
     const [jitsiToken, setJitsiToken] = useState<string>();
     const jitsiDomain = import.meta.env.VITE_JITSI_DOMAIN;
 
@@ -28,7 +27,7 @@ const VisioContainer = () => {
         const response = await getJitsiToken.execute(accessToken);
 
         if (response instanceof Error) {
-            return handleLogout();
+            return history.push('/home');
         }
 
         return setJitsiToken(response.token);
