c4d8b3afeaff9bfe54d6c649d3230b1c4386399f
roussill1 <hugo.roussillon@univ-lorraine.fr>
2024-07-12 10:02:37 +0200

    test matomo

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -107,6 +107,10 @@ const AppContext = () => {
         if (isReady) {
             getLanguage();
         }
+        var _mtm = window._mtm = window._mtm || [];
+        _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src='https://webstats.univ-lorraine.fr/js/container_ARY2lsO4.js'; s.parentNode?.insertBefore(g,s);
     }, [language, isReady]);
 
     if (error) {
