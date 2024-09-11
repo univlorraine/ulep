6b3b03557f05553fb86cad8d39495180b8f5c688
Marvyn <marvyn.orourke@thetribe.io>
2024-09-11 10:46:51 +0200

    👌 fix: Change how we display avatar on app chat

--- app/src/presentation/components/NetworkImage.tsx
+++ app/src/presentation/components/NetworkImage.tsx
@@ -4,13 +4,18 @@ import useGetMediaObject from '../hooks/useGetMediaObject';
 type NetworkImageProps = {
     id: string;
     placeholder?: string;
+    onErrorComponent?: () => JSX.Element;
 } & Omit<React.ComponentPropsWithRef<'img'>, 'src'>;
 
-const NetworkImage: React.FC<NetworkImageProps> = ({ id, placeholder, ...props }) => {
+const NetworkImage: React.FC<NetworkImageProps> = ({ id, placeholder, onErrorComponent, ...props }) => {
     const { loading, image, error } = useGetMediaObject({ id });
 
     if (loading) {
         return <IonSpinner color={'primary'}></IonSpinner>;
+    }
+
+    if ((error || !image) && onErrorComponent) {
+        return onErrorComponent();
     }
 
     return <img src={error ? placeholder : image} {...props} />;
