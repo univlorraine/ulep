58a8dc783445b736283d38f1be68a5a49f0de3ca
anne-line <anne-line.colombo@thetribe.io>
2024-10-21 16:42:49 +0200

    Fix: added missing imageURL on get by Id route

--- api/src/core/usecases/event/get-event.usecase.ts
+++ api/src/core/usecases/event/get-event.usecase.ts
@@ -1,17 +1,43 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { RessourceDoesNotExist } from 'src/core/errors';
 import {
   EventRepository,
   EVENT_REPOSITORY,
 } from 'src/core/ports/event.repository';
+import {
+  StorageInterface,
+  STORAGE_INTERFACE,
+} from 'src/core/ports/storage.interface';
+import { GetMediaObjectUsecase } from '../media';
 
 @Injectable()
 export class GetEventUsecase {
   constructor(
     @Inject(EVENT_REPOSITORY)
     private readonly eventRepository: EventRepository,
+    @Inject(STORAGE_INTERFACE)
+    private readonly storage: StorageInterface,
+    private readonly getMediaObjectUsecase: GetMediaObjectUsecase,
   ) {}
 
   async execute(id: string) {
-    return this.eventRepository.ofId(id);
+    const instance = await this.eventRepository.ofId(id);
+
+    if (!instance) {
+      throw new RessourceDoesNotExist();
+    }
+
+    const mediaObject = await this.getMediaObjectUsecase.execute({
+      id: `${instance.authorUniversity.id}/${instance.id}`,
+    });
+    const imageURL = mediaObject
+      ? await this.storage.temporaryUrl(
+          mediaObject.bucket,
+          mediaObject.name,
+          60 * 60 * 24,
+        )
+      : undefined;
+
+    return { ...instance, imageURL };
   }
 }
