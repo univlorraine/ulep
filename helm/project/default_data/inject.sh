5c11f22f33478c7e56d7ead0d1b4ed69ee2c021b
Colin Boulanger <colin.boulanger@thetribe.io>
2024-10-30 10:26:31 +0100

    doc: add comments to bash script files

--- helm/project/default_data/inject.sh
+++ helm/project/default_data/inject.sh
@@ -1,31 +1,32 @@
+# This script is launched after weblate installation.
+# Its goal is to create ulep project into weblate with json template files.
 echo '--------------------------------------------------------------------------------Inject default data----------------------------------------------------------------------------------'
 echo 'WEBLATE_SITE_DOMAIN : ' $WEBLATE_SITE_DOMAIN
+# Update container dependencies
 apk update
 apk add postgresql libpq curl jq
 
 echo "Wait for api ..."
-
+# Waiting for weblate api disponibilty
 while [[ "$(curl -k -s -o /dev/null -w ''%{http_code}'' https://$WEBLATE_SITE_DOMAIN)" != "200" ]]; do sleep 5; done
 
+#Get admin user token from database
 echo "Get admin token"
-
-
 ADMIN_TOKEN=$(psql -qtAX postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST/$PSQL_DATABASE -c "select key from authtoken_token where user_id=2")
 echo $ADMIN_TOKEN
 
 echo "Create project"
-
 project=$(cat /opt/scripts/projects.json)
 
 echo $project
-
+# In all of thoses files, modify the string %git_host% with the real git host.
 sed 's/%git_host%/'$GIT_HOST'/g' /opt/scripts/projects.json > /tmp/projects.json
 sed 's/%git_host%/'$GIT_HOST'/g' /opt/scripts/api_components.json > /tmp/api_components.json
 sed 's/%git_host%/'$GIT_HOST'/g' /opt/scripts/app_components.json > /tmp/app_components.json
 sed 's/%git_host%/'$GIT_HOST'/g' /opt/scripts/emails_components.json > /tmp/emails_components.json
 sed 's/%git_host%/'$GIT_HOST'/g' /opt/scripts/notifications_components.json > /tmp/notifications_components.json
 
-
+# create a project with weblate API
 PROJECT_RESPONSE=$(curl \
     -s \
     -d @/tmp/projects.json \
@@ -37,6 +38,7 @@ PROJECT_RESPONSE=$(curl \
 
 echo $PROJECT_RESPONSE | jq
 
+# create a api component with weblate API
 API_COMPONENT_RESPONSE=$(curl \
     -s \
     -k \
@@ -47,6 +49,7 @@ API_COMPONENT_RESPONSE=$(curl \
 
 echo $API_COMPONENT_RESPONSE | jq
 
+# create a app component with weblate API
 APP_COMPONENT_RESPONSE=$(curl \
     -s \
     -k \
@@ -57,6 +60,7 @@ APP_COMPONENT_RESPONSE=$(curl \
 
 echo $APP_COMPONENT_RESPONSE | jq
 
+# create a emails component with weblate API
 EMAILS_COMPONENT_RESPONSE=$(curl \
     -s \
     -k \
@@ -67,6 +71,8 @@ EMAILS_COMPONENT_RESPONSE=$(curl \
 
 echo $EMAILS_COMPONENT_RESPONSE | jq
 
+
+# create a notifications component with weblate API
 NOTIFICATIONS_COMPONENT_RESPONSE=$(curl \
     -s \
     -k \
--- helm/project/default_data/locales/init_git.sh
+++ helm/project/default_data/locales/init_git.sh
@@ -1,39 +1,39 @@
+# This script is launched just after git pod is available
+# It will create a repo and create the post-receive hook ( who will pushed its content into Minio)
+
 echo '- init git'
+# Update container dependencies
 apk add git openssh sshpass bash
 
-echo '- Wait for ssh daemon pod'
-
-#NOT NECESSARY IF LAUNCH FROM POD
-#until sshpass -p $GIT_PASSWORD ssh -o StrictHostKeyChecking=no git@$GIT_HOST -p 2222 2> /dev/null
-#  do
-#    sleep 5
-#  done
-
+# create a new repo 
 echo '- Init depo'
 sshpass -p $GIT_PASSWORD ssh -o StrictHostKeyChecking=no git@$GIT_HOST -p 2222 "mkdir /srv/git/locales.git"
 sshpass -p $GIT_PASSWORD ssh -o StrictHostKeyChecking=no git@$GIT_HOST -p 2222 "git-init --bare /srv/git/locales.git"
 
-
-
+# cloning this new repo (we can't modify a repo from the server)
 cd /tmp
 sshpass -p $GIT_PASSWORD git clone ssh://git@$GIT_HOST:2222/srv/git/locales.git
 cd locales
 
+# create folder into repo who has been cloned
 mkdir ./fr
 mkdir ./en
 mkdir ./de
 mkdir ./es
 mkdir ./zh
 
+# copy default translation files (default files are in locales folder).
 cp /opt/scripts/fr/*.json ./fr/
 cp /opt/scripts/en/*.json ./en/
 cp /opt/scripts/de/*.json ./de/
 cp /opt/scripts/es/*.json ./es/
 cp /opt/scripts/zh/*.json ./zh/
 
+# Add default identification
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
+# Commit and push translations files into develop & master
 git add .
 git commit -a -m "init repo"
 sshpass -p $GIT_PASSWORD git push --set-upstream origin master
@@ -41,52 +41,62 @@ sshpass -p $GIT_PASSWORD git push --set-upstream origin master
 git checkout -b develop
 sshpass -p $GIT_PASSWORD git push --set-upstream origin develop
 
-
+# Wait for weblate ssh keys : /tmp/weblate is a shared volume between weblate and git
 until [ -f /tmp/weblate/ssh/id_rsa.pub ]
 do
      sleep 5
 done
 echo "ID_RSA found"
 
+# Add ssh keys to git authorized keys
 echo '- Add ssh key to repo'
-
 chmod 600 /tmp/git/authorized_keys
 chmod 700 /tmp/git
-
 cat /tmp/weblate/ssh/id_rsa.pub >> /tmp/git/authorized_keys
 
 echo 'git ready to be used'
 
-
+# Install minio client
 wget https://dl.min.io/client/mc/release/linux-amd64/mc
 chmod +x mc
 mv mc /usr/local/bin/
 
-
+# Generate post-receive hook
 echo "- Generate post-receive hooks"
-
-
 GIT_SSH_COMMAND='GIT_SSH_COMMAND="sshpass -p '$GIT_PASSWORD' ssh -oStrictHostKeyChecking=no"'
 GIT_SSH_VARIANT='GIT_SSH_VARIANT="ssh"'
 GIT_TMP_PATH=/tmp/locales
 
+# Create post-receive file (the hook)
 touch /srv/git/locales.git/hooks/post-receive;
+# Allow it to be executed
 chmod a+x /srv/git/locales.git/hooks/post-receive;
 
 
 
+# Here we will write the content of post receive hook
 echo -e "#!/bin/sh
+# Create credentials with minio client
 mc alias set deploy http://$MINIO_HOST:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+
+# remove old git clonin
 cd /tmp/
 rm -rf /tmp/locales
+
+# clone the repo
 $GIT_SSH_COMMAND $GIT_SSH_VARIANT git -C /tmp clone --branch develop ssh://git@$GIT_HOST:2222/srv/git/locales.git
+
+# wait cloning end
 until [ -f /tmp/locales/fr/api.json ]
 do
 sleep 5
 done
+
+# upload content of cloning to minio
 mc mirror --overwrite $GIT_TMP_PATH "deploy/i18n" && mc anonymous set public "deploy/i18n"
 " >> /srv/git/locales.git/hooks/post-receive;
 
+# Change owning of post-receive hooks
 chown git:git /srv/git/locales.git/hooks/post-receive;
 
 echo '- Repo ready';
