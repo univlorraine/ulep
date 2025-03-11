0195972e6148807d911b5c40fdce99d1bb8e727b
anne-line <anne-line.colombo@thetribe.io>
2025-03-11 11:43:54 +0100

    feat: Update central university edito translations during instance update

--- api/src/core/usecases/instance/update-instance.usecase.ts
+++ api/src/core/usecases/instance/update-instance.usecase.ts
@@ -2,9 +2,17 @@ import { Inject, Injectable } from '@nestjs/common';
 import { RessourceDoesNotExist } from 'src/core/errors';
 import { EditoMandatoryTranslations } from 'src/core/models/Instance.model';
 import {
+  EditoRepository,
+  EDITO_REPOSITORY,
+} from 'src/core/ports/edito.repository';
+import {
   InstanceRepository,
   INSTANCE_REPOSITORY,
 } from 'src/core/ports/instance.repository';
+import {
+  UniversityRepository,
+  UNIVERSITY_REPOSITORY,
+} from 'src/core/ports/university.repository';
 
 export class UpdateInstanceCommand {
   name?: string;
@@ -27,6 +35,10 @@ export class UpdateInstanceUsecase {
   constructor(
     @Inject(INSTANCE_REPOSITORY)
     private readonly instanceRepository: InstanceRepository,
+    @Inject(EDITO_REPOSITORY)
+    private readonly editoRepository: EditoRepository,
+    @Inject(UNIVERSITY_REPOSITORY)
+    private readonly universityRepository: UniversityRepository,
   ) {}
 
   async execute(command: UpdateInstanceCommand) {
@@ -39,6 +51,37 @@ export class UpdateInstanceUsecase {
     const newInstance = await this.instanceRepository.update({
       id: instance.id,
       ...command,
+    });
+
+    // Update the central university edito translations
+    const centralUniversity =
+      await this.universityRepository.findUniversityCentral();
+
+    const edito = await this.editoRepository.findByUniversityId(
+      centralUniversity.id,
+    );
+
+    const newTranslations = command.editoCentralUniversityTranslations
+      ? command.editoCentralUniversityTranslations.map((translation) => {
+          const existingTranslation = edito.translations.find(
+            (t) => t.languageCode === translation,
+          );
+          return {
+            languageCode: translation,
+            content: existingTranslation?.content || '',
+          };
+        })
+      : [];
+
+    newTranslations.push(
+      edito.translations.find((t) => t.languageCode === 'en'),
+    );
+
+    await this.editoRepository.update({
+      id: edito.id,
+      content: edito.content,
+      languageCode: edito.languageCode,
+      translations: newTranslations,
     });
 
     return newInstance;
