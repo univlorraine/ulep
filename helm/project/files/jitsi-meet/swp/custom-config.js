bf7532b7b2db2e45f5628c1b7c34ac90dfd139a9
Marvyn <marvyn.orourke@thetribe.io>
2025-07-10 10:51:02 +0200

    feat: enhance Jitsi configuration to support moderator features
    - Updated configuration to allow all users to be moderators, enabling access to the "End Meeting" button.
    - Changed subject string to use double quotes for consistency.

--- helm/project/files/jitsi-meet/swp/custom-config.js
+++ helm/project/files/jitsi-meet/swp/custom-config.js
@@ -1,5 +1,10 @@
-config.subject = 'ETANDEM';
+config.subject = "ETANDEM";
 config.doNotStoreRoom = true;
 config.prejoinPageEnabled = false;
 config.enableWelcomePage = false;
 config.enableClosePage = false;
+
+// Configuration pour permettre à tous les utilisateurs d'être modérateurs
+// Cela leur donnera accès au bouton "End Meeting" dans l'interface Jitsi
+config.everyoneIsModerator = true;
+config.autoGrantModerator = true;
